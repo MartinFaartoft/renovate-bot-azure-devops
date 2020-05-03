4f9ba4a70acbfcb66a8f9752db741a37c1e62aa1
Martin Faartoft <martin.faartoft@gmail.com>
2020-05-03 13:57:12 +0200

    endpoint in env var

--- renovate-config.js
+++ renovate-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //endpoint: 'https://martinfaartoft0788.visualstudio.com/',
+    //endpoint: 'https://dev.azure.com/{organizationName}/', // You can specify the URI here, or read it from the Azure DevOps pipeline (see renovate-pipeline.yaml).
     platform: 'azure',
     logLevel: 'debug',
 };
